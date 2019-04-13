ee1d616f11445692d4c19c9269951f13de008467
admin <brotik13@gmail>
2019-04-14 00:58:04 +0300

    add css bootstrap

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
-from flask import Flask
+import uuid
 
-from app.goods import ProductManager, Product
+from flask import Flask, request, render_template
+
+from app.goods import ProductManager, Product, SaleManager
 
 
 def start():
@@ -15,6 +17,32 @@ def start():
     product_manager.add(Product('MacBook PRO', price=99900, amount=5))
     product_manager.add(Product('Apple Watch 3', price=17900, amount=7))
 
+    sale_manager = SaleManager()
+    product_saved = False
+    sale_alert = None
 
+    @app.route('/')
+    def index():
+        sort_field = request.args.get('sort_order')
+        if sort_field not in ['name', 'price', 'amount']:
+            sort_field = 'name'
+
+        sort_order = request.args.get('sort_order')
+        if sort_order not in ['asc', 'desc']:
+            sort_order = 'asc'
+
+        product_manager.sort_items(sort_field, sort_order)
+
+        search = request.args.get('search')
+        if search:
+            products = product_manager.search_by_name(search)
+        else:
+            products = product_manager.items
+            search = ''
+
+        empty_id = uuid.UUID(int=0)
+
+        return render_template('index.html', products=products, empty_id=empty_id, search=search,
+                               sort_order=sort_order, sort_field=sort_field)
 
 
